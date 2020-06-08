1136357138d7e706a5f38dd3dc908aba689e51ee
Waleed A. Afifi <waleedafifich@gmail.com>
2020-06-08 14:13:20 +0300

    Creat prompt in app.js that ask user 5 question, create header and main and footer and style them from style.css, add reset css file

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use-strict';
+
+var userName = prompt('Please tell us your name ^_^', 'Waleed');
+// console.log('The user name is', userName);
+alert('The user name is ' + userName);
+
+var Question1 = prompt('Do you like to see site owner in person? y/yes ','y').toLowerCase();
+// console.log('The user has answer question 1', Question1);
+while( Question1 != 'y' && Question1 != 'yes' && Question1 != 'n' && Question1 != 'no') {
+    Question1 = prompt('Do you like to see site owner in person? y/yes ','y').toLowerCase();
+}
+alert('Your answer question 1 ' + Question1);
+
+var Question2 = prompt('Did this site helps you to get whats you are looking for? y/yes','y').toLowerCase();
+// console.log('The user has answer question 2', Question2);
+while( Question2 !== 'y' && Question2 !== 'yes' && Question2 !== 'n' && Question2 !== 'no') {
+    Question2 = prompt('Did this site helps you to get whats you are looking for? y/yes','y').toLowerCase();
+}
+alert('Your answer question 2 ' + Question2);
+
+var Question3 = prompt('What if we gave you a coupon reduce some product price, will it be good? y/yes','y').toLowerCase();
+// console.log('The user has answer question 3', Question3);
+while( Question3 !== 'y' && Question3 !== 'yes' && Question3 !== 'n' && Question3 !== 'no') {
+    Question3 = prompt('What if we gave you a coupon reduce some product price, will it be good? y/yes','y').toLowerCase();
+}
+alert('Your answer question 3 ' + Question3);
+
+var Question4 = prompt('Some users don\'t like to buy online are you one of them? y/yes','y').toLowerCase();
+// console.log('The user has answer question 4', Question4);
+while( Question4 !== 'y' && Question4 !== 'yes' && Question4 !== 'n' && Question4 !== 'no') {
+    Question4 = prompt('Some users don\'t like to buy online are you one of them? y/yes','y').toLowerCase();
+}
+alert('Your answer question 4 ' + Question4);
+
+var Question5 = prompt('Are you a cat person? y/yes','y').toLowerCase();
+// console.log('The user has answer question 5', Question5);
+while( Question5 !== 'y' && Question5 !== 'yes' && Question5 !== 'n' && Question5 !== 'no' ) {
+    Question5 = prompt('Are you a cat person? y/yes','y').toLowerCase();
+}
+alert('Your answer question 5 ' + Question5);
+
+alert(userName + '\nThank for your kind and help us with survey \nYour coupon code is FGremm32 \n Have a nice day');
